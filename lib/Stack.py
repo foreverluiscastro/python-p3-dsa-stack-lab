9afbca02220567d11cb444c0cf8fb028b6463a2c
Luis <foreverluiscastro@gmail.com>
2023-02-15 18:52:26 -0500

    Done with the deliverables

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,39 @@
+import ipdb
+
 class Stack:
 
     def __init__(self, items = [], limit = 100):
         pass
+        self.items = items[0:limit]
+        self.limit = limit
 
     def isEmpty(self):
         pass
+        return self.size() == 0
 
     def push(self, item):
         pass
+        self.items.insert(0, item)
 
     def pop(self):
         pass
+        return self.items.pop(0)
 
     def peek(self):
         pass
     
     def size(self):
         pass
+        return len(self.items)
 
     def full(self):
         pass
+        return self.size() >= self.limit
 
     def search(self, target):
         pass
+        for idx, value in enumerate(self.items):
+            if target == value:
+                return idx
+        
+        return -1
